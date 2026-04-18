7be5361f70b8f6fc949e9b33bdd0cb63b2d696d1
Salvatore-droid <geniusokwemba53@gmail.com>
2026-04-18 23:52:02 +0300

    modified the backend url endpoint to production

--- vite.config.ts
+++ vite.config.ts
@@ -173,7 +173,7 @@ export default defineConfig({
     host: true,
     proxy: {
       "/api": {
-        target: "http://localhost:8000",
+        target: "https://geniusgaurdbackend.onrender.com",
         changeOrigin: true,
       },
     },
